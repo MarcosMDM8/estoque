d2300131b96d2f15485f91dd8d26735b73227043
MarcosMDM8 <tecnicocomputer.sistemas@gmail.com>
2025-04-17 20:41:58 -0300

    Update Tservice-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,25 @@
-const CACHE_NAME = "estoque-v1";
-const FILES_TO_CACHE = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-// Instala e armazena arquivos
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
+  console.log('Service Worker Instalado');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open('estoque-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+        '/style.css',
+        '/script.js'
+      ]);
+    })
   );
 });
 
-// Ativa o Service Worker
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
-});
-
-// Busca no cache se offline
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
