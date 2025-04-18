64b2d7e7fa7fc1d7df1101d2bef3563386514f87
MarcosMDM8 <tecnicocomputer.sistemas@gmail.com>
2025-04-17 21:18:11 -0300

    UpdateFFFF service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,13 @@
 self.addEventListener('install', (event) => {
-  console.log('Service Worker Instalado');
   event.waitUntil(
-    caches.open('estoque-cache').then((cache) => {
+    caches.open('estoque-pwa-cache').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
         '/manifest.json',
         '/icon-192.png',
         '/icon-512.png',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-        '/style.css',
-        '/script.js'
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
       ]);
     })
   );
@@ -18,8 +15,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
